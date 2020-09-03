c986dd02eb2084c55e14713f09d0aee34be1a4d1
axg1247 <Merogithub123>
2020-09-03 12:54:14 -0400

    first

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@ void setup() {
   // put your setup code here, to run once:
 }
 
+void control() {
+  // put your control code here, to run once:
+}
+
+
 void loop() {
   // put your main code here, to run repeatedly:
 }
